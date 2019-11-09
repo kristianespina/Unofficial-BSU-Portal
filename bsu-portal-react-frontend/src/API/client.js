62b6c9d45196d6574a4cc54a51ef09b4c36e564b
Kristian Espina <onelespina@gmail.com>
2019-11-09 21:26:39 +0800

    defaulting API uri to heroku

--- bsu-portal-react-frontend/src/API/client.js
+++ bsu-portal-react-frontend/src/API/client.js
@@ -1,7 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
 const graphQLClient = new ApolloClient({
-    uri: "http://localhost:4000/graphql"
+    //uri: "http://localhost:4000/graphql"
+    uri: "https://unofficial-bsu-portal-graphql.herokuapp.com/graphql"
 });
 
 export default graphQLClient
